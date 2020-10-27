1f2357f6d1da4f6a080aebbe558a8e3a70aa484b
Josué Camelo <josueprg@gmail.com>
2020-10-27 10:22:07 -0300

    Ajustes de Layouts

--- resources/views/vendor/jetstream/components/authentication-card.blade.php
+++ resources/views/vendor/jetstream/components/authentication-card.blade.php
@@ -1,12 +1,4 @@
 <div id="register" class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0">
-{{--    <div>--}}
-{{--        {{ $logo }}--}}
-{{--    </div>--}}
-
-{{--    <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">--}}
-{{--        {{ $slot }}--}}
-{{--    </div>--}}
-
     <div class="container-form bg-white max-full mx-auto p-8 md:p-12 my-10 rounded-lg shadow-2xl">
         <div class="logo">
             {{ $logo }}
